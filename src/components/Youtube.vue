8203dbe644f28a8c5103cdf9a49b9db475b9fbb4
Francisco Carmona <flcarmonac@yahoo.com>
2024-11-05 01:30:49 -0300

    Testing Shadows

--- src/components/Youtube.vue
+++ src/components/Youtube.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flex flex-wrap gap-4 justify-center max-w-screen-xl mx-auto my-5">
-    <div class="card w-96 bg-base-100 shadow-red-700 shadow-lg">
+    <div class="card w-96 bg-base-100 shadow-red-700 shadow-sm">
       <iframe class="w-full aspect-video rounded-xl" src="https://www.youtube.com/embed/Y-JJLG4AXFY?si=9LAR4bzxzVUJmDKb"></iframe>
     </div>
-    <div class="card w-96 bg-base-100 shadow-red-700 shadow-lg">
+    <div class="card w-96 bg-base-100 shadow-red-700 shadow-md">
       <iframe class="w-full aspect-video rounded-xl" src="https://www.youtube.com/embed/MvSWyJPb-to?si=liiOb9jO9uVJ4OkI"></iframe>
     </div>
     <div class="card w-96 bg-base-100 shadow-red-700 shadow-lg">
