676bb216db6aeaf98ea4a1e9f80415ec72dd74d0
Francisco Carmona <flcarmonac@yahoo.com>
2024-11-05 21:06:18 -0300

    Update

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import Banner from '@/components/Banner.vue';
 import Titles from '@/components/Titles.vue';
 import Youtube from '@/components/Youtube.vue';
