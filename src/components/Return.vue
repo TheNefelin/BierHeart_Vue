ec62f5c77bab2446ba3f703a5aad5834c41fb1a6
Francisco Carmona <flcarmonac@yahoo.com>
2024-11-05 21:00:46 -0300

    update

--- src/components/Return.vue
+++ src/components/Return.vue
@@ -1,5 +1,5 @@
 <template>
-  <a class="fixed bottom-4 right-2 cursor-pointer" href="#">
+  <a class="fixed z-20 bottom-4 right-2 cursor-pointer" href="#">
     <img class="h-14" src="@/assets/images/pentagram.png">
   </a>
 </template>
