ee33ef8505f69a70e8f67d989de37a2df1bd9f32
Francisco Carmona <flcarmonac@yahoo.com>
2024-11-07 08:55:10 -0300

    update

--- src/components/Youtube.vue
+++ src/components/Youtube.vue
@@ -7,7 +7,7 @@
       <iframe class="w-full aspect-video rounded-xl" src="https://www.youtube.com/embed/Y-JJLG4AXFY?si=9LAR4bzxzVUJmDKb"></iframe>
     </div>
     <div class="card w-96 bg-base-100 shadow-red-700 shadow-md">
-      <iframe class="w-full aspect-video rounded-xl" src="https://www.youtube.com/embed/MvSWyJPb-to?si=liiOb9jO9uVJ4OkI"></iframe>
+      <iframe class="w-full aspect-video rounded-xl" src="https://www.youtube.com/embed/asZp6USFMVs?si=jqbMYbUA6pyNmgGS"></iframe>
     </div>
     <div class="card w-96 bg-base-100 shadow-red-700 shadow-md">
       <iframe class="w-full aspect-video rounded-xl" src="https://www.youtube.com/embed/dCILv_kU9Tg?si=0mU1P4ixzAK6iGwl"></iframe>
